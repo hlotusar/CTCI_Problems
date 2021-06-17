2610cc3f9eff739e47e7d6427a8c534f9a44dc17
Tusarjyoti Das <tusarjyotidas@gmail.com>
2021-06-17 11:36:19 +0530

    linked list problem 2.1

---
+++ TestConsoleSoln/TestConsole/DSA/SinglyLinkedList.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestConsole.DSA
+{
+    public class SinglyLinkedList<T>
+    {
+        //public int Count { get; set; }
+
+        public Node<T> Head { get; set; }
+        
+        public Node<T> AddNodeAtHead(Node<T> node)
+        {
+            if(Head != null)
+            {
+                node.Next = this.Head;
+            }
+            Head = node;
+           // Count++;
+            return Head;
+        }
+
+        public static void PrintLinkedList(Node<T> head)
+        {
+            var node = head;
+            while(node != null)
+            {
+                Console.WriteLine(node.Data);
+                node = node.Next;
+            }
+        }
+
+        public Node<T> RemoveNodeAtHead(Node<T> node)
+        {
+            if (Head != null)
+            {
+                Head = Head.Next;
+                //Count--;
+            }
+            return Head;
+        }
+    }
+
+    public class Node<T>
+    {
+        public T Data { get; set; }
+
+        public Node<T> Next { get; set; }
+
+        public Node(T data, Node<T> next = null)
+        {
+            Data = data; Next = next;
+        }
+    }
+}
---
+++ TestConsoleSoln/TestConsole/Problems/LinkedLists.cs
@@ -0,0 +1,31 @@
+﻿using TestConsole.DSA;
+
+namespace TestConsole
+{
+    public class LinkedLists<T>
+    {
+        /// <summary>
+        /// 2.1 - Remove duplicates from a unsorted singly linked list
+        /// Bruteforce by taking buffer - O(N^2)
+        /// </summary>
+        /// <param name="Head"></param>
+        /// <returns></returns>
+        public void RemoveDuplicates(Node<T> Head)
+        {
+            var node = Head;
+            while(node!=null)
+            {
+                var bufferNode = node;
+                while(bufferNode.Next != null)
+                {
+                    if (node.Data.Equals(bufferNode.Next.Data))
+                    {
+                        bufferNode.Next = bufferNode.Next.Next;
+                    }
+                    else bufferNode = bufferNode.Next;
+                }
+                node = node.Next;
+            }
+        }
+    }
+}
